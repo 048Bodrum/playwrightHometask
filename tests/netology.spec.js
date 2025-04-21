949e033fb21c639633489e3c3b65578ded70761d
Bilen Liubov <bilenliubov48@mail.ru>
2025-04-21 16:43:44 +0300

    changes in files

--- tests/netology.spec.js
+++ tests/netology.spec.js
@@ -10,14 +10,5 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Пароль' }).click();
   await page.getByRole('textbox', { name: 'Пароль' }).fill(password);
   await page.getByTestId('login-submit-btn').click();
-  await expect(page.locator("[data-testid= 'menu-userface']")).toContainText("ЛБ");
-
-  await page.goto('https://netology.ru');
-  await page.getByRole('link', { name: 'Войти' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).fill(email);
-  await page.getByRole('textbox', { name: 'Пароль' }).click();
-  await page.getByRole('textbox', { name: 'Пароль' }).fill(invalidPassword);
-  await page.getByTestId('login-submit-btn').click();
-  await expect(page.locator("[data-testid= 'login-error-hint']")).toContainText("Вы ввели неправильно логин или пароль.");
+  await expect(page.locator("[data-testid='menu-userface']")).toContainText("ЛБ");
 });
